93d8c26ae20bba9599da4d472fdd304fb742dabd
LeoC85 <leandrocasas@hotmail.com>
2020-08-15 21:13:05 +1000

    Update githubapi.py

--- githubapi.py
+++ githubapi.py
@@ -7,7 +7,7 @@ payload = ""
 headers = {
     'Authorization': "Bearer <Your Token",
     'cache-control': "no-cache",
-    'Postman-Token': "925dcaab-f487-4d56-9ec9-4e94f29e5007"
+    'Postman-Token': "<Take from postman raw view>"
     }
 
 response = requests.request("GET", url, data=payload, headers=headers)
